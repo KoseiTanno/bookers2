ef2f9ced997df6dcb7b80e79063a2f402c3a2f36
EC2 Default User <ec2-user@ip-172-31-9-84.ap-northeast-1.compute.internal>
2024-06-28 06:15:28 +0000

    Bookers2

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,14 @@
+// app/javascript/packs/application.js
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application";
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
